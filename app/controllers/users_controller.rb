da4d0b65837f3773830fecb5c96a1420f50710f8
Jared Hodgkins <phusiondev@gmail.com>
2017-11-02 12:41:01 -0400

    boilerplate on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,56 @@
 class UsersController < ApplicationController
+  before_action :setup_user, only: [:show, :edit, :update, :destroy]
+
   def index
+
   end
 
   def show
+
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
   end
 
   def edit
+
+  end
+
+  def update
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def setup_user
+    @user = User.find_by id: params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :user_name,
+      :email,
+      :email_confirmation,
+      :password,
+      :password_confirmation,
+    )
   end
 end
