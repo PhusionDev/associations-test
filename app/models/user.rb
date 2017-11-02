5f4381cb6f9af3b1d0aef9afacab657f4277f046
Jared Hodgkins <phusiondev@gmail.com>
2017-11-02 13:02:56 -0400

    initial new user creation form and functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_one :profile
   has_many :events
   has_many :event_applications
   accepts_nested_attributes_for :profile
+
+  validates :email, confirmation: true
+  validates :password, confirmation: true
 end
